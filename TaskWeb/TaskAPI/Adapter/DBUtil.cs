a5006ebe11de1819e4f1829d988f76dd83c5a176
Chengwei Xu <rwayxu@hotmail.com>
2018-08-13 22:42:00 +0800

    update

--- TaskWeb/TaskAPI/Adapter/DBUtil.cs
+++ TaskWeb/TaskAPI/Adapter/DBUtil.cs
@@ -1,9 +1,14 @@
 ﻿using SampleUtils.LogUtils;
 using System;
+using System.Data;
 
 namespace DBAgent.Adapter
 {
     internal class DBUtil : LogHelper, IDBAdapter
     {
+        DataTable IDBAdapter.SQLExecuteReturnTable()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
