daaeba52f535df7ab09ac855c2d8c7a805ea9f31
Chengwei Xu <rwayxu@hotmail.com>
2018-09-02 22:44:42 +0800

    update

--- Client/Task/src/components/aematerial.vue
+++ Client/Task/src/components/aematerial.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-  <mu-form :model="form" :label-position="'right'" label-width="100">
+  <mu-form :model="item" :label-position="'right'" label-width="100">
     <mu-form-item prop="input" label="姓名">
-      <mu-text-field v-model="form.input"></mu-text-field>
+      <mu-text-field v-model="form.name"></mu-text-field>
     </mu-form-item>
     <mu-form-item prop="date" label="生日">
-      <mu-date-input v-model="form.date" type="date" actions></mu-date-input>
+      <mu-date-input v-model="form.brithday" type="date" actions></mu-date-input>
     </mu-form-item>
     <mu-form-item prop="radio" label="性别">
-      <mu-radio v-model="form.radio" value="male" label="男"></mu-radio>
-      <mu-radio v-model="form.radio" value="female" label="女"></mu-radio>
+      <mu-radio v-model="form.gender" value="male" label="男"></mu-radio>
+      <mu-radio v-model="form.gender" value="female" label="女"></mu-radio>
     </mu-form-item>
     <mu-form-item prop="textarea" label="备注">
-      <mu-text-field multi-line :rows="3" :rows-max="6" v-model="form.textarea"></mu-text-field>
+      <mu-text-field multi-line :rows="3" :rows-max="6" v-model="form.remarks"></mu-text-field>
     </mu-form-item>
     <mu-form-item label="尺寸附件">
       <fileupload />
@@ -32,8 +32,8 @@ export default {
         'Option 5', 'Option 6', 'Option 7', 'Option 8',
         'Option 9', 'Option 10'
       ],
-      form: {
-        input: '',
+      item: {
+        name: '',
         select: '',
         date: '',
         radio: '',
