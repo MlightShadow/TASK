079c9ae024cff6b430019485bc5fa175fc77a862
Chengwei Xu <rwayxu@hotmail.com>
2018-09-11 13:10:01 +0800

    add api

--- Client/Task/src/main.js
+++ Client/Task/src/main.js
@@ -6,9 +6,11 @@ import router from './router'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 import 'material-design-icons/iconfont/material-icons.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 Vue.use(MuseUI)
+Vue.use(axios)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
